172b7352e043466530361058d87f8c005537eab2
Remo Laubacher <remo.laubacher@gmail.com>
2016-12-23 14:47:49 +0100

    remove module export

--- src/markerclusterer.js
+++ src/markerclusterer.js
@@ -1391,5 +1391,3 @@ Cluster.prototype['getMarkers'] = Cluster.prototype.getMarkers;
 ClusterIcon.prototype['onAdd'] = ClusterIcon.prototype.onAdd;
 ClusterIcon.prototype['draw'] = ClusterIcon.prototype.draw;
 ClusterIcon.prototype['onRemove'] = ClusterIcon.prototype.onRemove;
-
-module.exports = MarkerClusterer;
