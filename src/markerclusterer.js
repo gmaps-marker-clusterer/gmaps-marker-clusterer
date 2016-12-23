c654c38eca28e8fe4b3fe46492e9a9fe51f0c294
Remo Laubacher <remo.laubacher@gmail.com>
2016-12-23 14:23:30 +0100

    fix clusterlick event firing

--- src/markerclusterer.js
+++ src/markerclusterer.js
@@ -1137,6 +1137,7 @@ ClusterIcon.prototype.onAdd = function() {
 
   var that = this;
   var isDragging = false;
+  var isMouseDown = false;
   google.maps.event.addDomListener(this.div_, 'click', function(event) {
     // Only perform click when not preceded by a drag
     if (!isDragging) {
@@ -1145,9 +1146,16 @@ ClusterIcon.prototype.onAdd = function() {
   });
   google.maps.event.addDomListener(this.div_, 'mousedown', function() {
     isDragging = false;
+    isMouseDown = true;
+  });
+  google.maps.event.addDomListener(this.div_, 'mouseup', function() {
+    isDragging = false;
+    isMouseDown = false;
   });
   google.maps.event.addDomListener(this.div_, 'mousemove', function() {
-    isDragging = true;
+    if (isMouseDown) {
+      isDragging = true;
+    }
   });
 };
 
